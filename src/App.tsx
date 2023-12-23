154ac510bbbdded7f8401ebcd735fae020ca486c
me <cokevore@gmail.com>
2023-12-22 16:28:36 -0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Favorite Books</h1>
+      <p>A thing to list your favorite books!</p>
+        
+    </>
+  )
+}
+
+export default App
